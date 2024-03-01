eac5b15f3dd3c17420061d547d95ce0f3863dabb
Minh Tran <50918867+MinhTran0311@users.noreply.github.com>
2024-03-02 00:45:47 +0200

    213

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,17 +1,33 @@
+import 'package:flutter_application_1/screens/home/cubit/home_cubit.dart';
 import 'package:flutter_application_1/screens/home/home_screen.dart';
+import 'package:flutter_application_1/screens/question_screen/cubit/question_cubit.dart';
 import 'package:flutter_application_1/screens/question_screen/question_screen.dart';
 import 'package:flutter_application_1/screens/statistic/statistic_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
   routes: [
-    GoRoute(path: '/', builder: (context, state) => HomeScreen()),
+    GoRoute(
+        path: '/',
+        builder: (context, state) {
+          final cubit = HomeCubit();
+          return BlocProvider(
+              create: (context) => cubit,
+              child: HomeScreen(
+                cubit: cubit,
+              ));
+        }),
     GoRoute(path: '/statistic', builder: (context, state) => StatisticScreen()),
     GoRoute(
         path: '/question/:id',
         builder: (context, state) {
-          return QuestionScreen(
-              topicId: int.parse(state.pathParameters['id']!));
+          final cubit = QuestionCubit();
+          return BlocProvider(
+            create: (context) => cubit,
+            child: QuestionScreen(
+                topicId: int.parse(state.pathParameters['id']!), cubit: cubit),
+          );
         }),
   ],
 );
